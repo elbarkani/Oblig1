f88be81fd17cf78b74d78e6926e6e555c1e956c0
Yassin <yassin.el@live.no>
2018-09-03 15:46:26 +0200

    Test

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -4,25 +4,44 @@ public class Oblig1 {
 
     public static void main(String[] args){
 
-        int[] a = {4, 3, 1, 5, 6, 10, 9, 7, 8, 2};
+        int[] a = {4, 3, 1, 5, 6, 6, 10, 9, 7, 7, 8, 2};
         int[] b = {3, 3, 4, 5, 5, 6, 7, 7, 7, 8};
 
         System.out.println("Antall ulike: " + antallUlikeSortert(b));
-        antallUlikeSortert(b);
-        ombyttinger(a);
-        maks(a);
+        System.out.println("Antall ulike: " + antallUlikeUsortert(a));
+        //ombyttinger(a);
+        //maks(a);
 
+    }
+
+    public static int antallUlikeUsortert(int[] a){
+
+        if (a.length==0) {
+            return 0;
+        }
+
+        int ulike = 1;
+        for (int i=0; i<a.length-1; i++){
+            if (a[i] != a[i+1]) {
+                ulike++;
+            }
+        }
+        return ulike;
     }
 
     public static int antallUlikeSortert(int[] a){
 
-        int ulike = 1;
+        if (a.length==0){
+            return 0;
+        }
 
         for (int i=0; i<a.length-1; i++){
             if (a[i] > a[i+1]) {
                 throw new IllegalArgumentException("Må ha stigende rekkefølge i tabell");
             }
         }
+
+        int ulike = 1;
 
         for (int i=0; i<a.length-1; i++){
             if (a[i] != a[i+1]) {
